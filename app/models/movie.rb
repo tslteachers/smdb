689c02f7f0d4455b1c9c55dd3de39633860e2d25
Raghu Betina <rbetina@gmail.com>
2013-07-29 14:06:23 -0500

    Introduced the shortcuts for association methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,30 @@
 class Movie < ActiveRecord::Base
   # belongs to director
-  def director
-    return Director.find_by_id(self.director_id)
-  end
+
+  # The short Rails way
+  belongs_to :director
+
+  # The full Rails way
+  # belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
+
+  # The hand-written way
+  # def director
+  #   return Director.find_by_id(self.director_id)
+  # end
 
   # has many roles
-  def roles
-    return Role.where(:movie_id => self.id)
-  end
+
+  has_many :roles
+
+  # def roles
+  #   return Role.where(:movie_id => self.id)
+  # end
 end
+
+
+
+
+
+
+
+
