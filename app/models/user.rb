c2bac507730c6e95551359b32b31795f60e06f9f
Raghu Betina <rbetina@gmail.com>
2013-08-02 14:14:19 -0500

    Completed vote forms everywhere

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :votes
   has_many :movies, :through => :votes
+
+  validates :email, :uniqueness => true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to :movie
   belongs_to :user
+
+  validates :user_id, :uniqueness => { :scope => :movie_id, :message => "has already voted for that movie" }, :presence => true
+  validates :movie, :presence => true
 end
